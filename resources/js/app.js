ab0da99ed3889d7caeb24e0ddd68edd4e087c629
Juan Carlos Marchant <juan.marchant@gmail.com>
2021-10-28 22:43:57 -0300

    add layout fro frontend

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
 require('./bootstrap');
+
+import Alpine from 'alpinejs';
+window.Alpine = Alpine;
+Alpine.start();
+
+// TailwindCSS Dark Mode
+// buttons switch mode
+document.querySelectorAll(".setMode").forEach(item => 
+    item.addEventListener("click", () => {
+        let htmlClasses = document.querySelector('html').classList;
+        if(localStorage.theme == 'dark') {
+            htmlClasses.remove('dark');
+            localStorage.theme = ''
+        } else {
+            htmlClasses.add('dark');
+            localStorage.theme = 'dark';
+        }
+    })
+)
+// Dark mode persist
+if (localStorage.theme === 'dark' || (!'theme' in localStorage && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.querySelector('html').classList.add('dark')
+} else if (localStorage.theme === 'dark') {
+    document.querySelector('html').classList.add('dark')
+}
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('pages.home');
+});
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
